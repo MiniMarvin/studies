53dc6744015e0a849cdb43f30b9a0f7af6ad5359
minimarvin <caiogomes34@gmail.com>
2017-08-16 14:04:13 +0000

    Resolvendo o problema 2 da lista novamente, por ter dado erro.

--- algorithms/lists/list_0/fib.cpp
+++ algorithms/lists/list_0/fib.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <array>
 
-#define NUM_LIMIT 100000
+#define NUM_LIMIT 100000 // 10^5
 
 using namespace std;
 
+typedef unsigned long int uli;
+
 unsigned long int fib(unsigned int x, unsigned long int* fib_arr);
+void insertion_sort(uli *arr, int size, int order);
+void printArr(uli *arr, int size);
 
 // TODO: ordenar o array
 
 int main() {
 
 	unsigned long int fib_a;
-	unsigned long int fib_arr[100000] = {0};
+	unsigned long int fib_arr[11000] = {0}; // In the maximum have 10^5 + 10^6 values
+	unsigned long int arr[11000] = {0}; // In the maximum have 10^5 + 10^6 values
 	unsigned long int u;
 	int t_cases = 0;
 	int in_a, in_b, end;
@@ -21,26 +27,33 @@ int main() {
 	fib_arr[1] = 1;
 
 	cin >> t_cases;
+	
 	for(int i = 1; i <= t_cases; i++) {
-
+		
 		cin >> in_a;
 		cin >> in_b;
 	
 		end = in_a + in_b;
-
 		cout << "Case "	<< i << ": ";
+		
 	
 		for(int j = in_a; j <= end; j++) {
-			cout << fib(j, fib_arr);
-			cout << " ";
+			
+			arr[j - in_a] = fib(j, fib_arr);
+			//cout << " ";
+			
+			//arr[ j - in_a] = fib_arr[j-1];
 		}
-		cout << endl;
+		
+		insertion_sort(arr, in_b + 1, -1);
+		printArr(arr, in_b + 1);
+		
+		//delete arr;
 	}
 	
 
 	return 0;
 }
-
 
 
 unsigned long int fib(unsigned int x, unsigned long int* fib_arr) {
@@ -67,3 +80,28 @@ unsigned long int fib(unsigned int x, unsigned long int* fib_arr) {
 	return fib_arr[x-1];
 }
 
+
+
+void insertion_sort(uli *arr, int size, int order) {
+    
+    // increase order
+    for(int i = 1; i < size; i++) {
+        
+        int key = arr[i]; // make the actual value as comparison key
+        int j = i-1; // start analysing the value before the key one
+        
+        for(;j >= 0 && (order >= 0 ? arr[j] < key : arr[j] > key); j--) {
+            arr[j+1] = arr[j]; // put the array elements one element to the front
+            arr[j] = key; // garant that the key is going to be placed in the correct positioin
+        }
+    }
+}
+
+void printArr(uli *arr, int size) {
+	if(size > 100) size = 100;
+	
+	for (int i = 0; i < size ; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
---
+++ algorithms/lists/list_0/fib_1.cpp
@@ -0,0 +1,126 @@
+#include <iostream> 
+
+using namespace std;
+
+long int fib(int pos);
+long int fibArr(int begin, int end, long int *arr);
+void printArr(long int *arr, int size);
+
+
+// TODO: recebe todos os valores e calcula os valores de fibonacci somente necessários para aquelas operações
+int main() {
+	
+	int in_num = 0;
+	
+	cin >> in_num;
+	
+	// create the arrays
+	long int *in_a = new long int[in_num];
+	long int *in_b= new long int[in_num];
+	
+	cin >> in_a[0];
+	cin >> in_b[0];
+	
+	long int lower = in_a[0];
+	long int bigger = in_a[0] + in_b[0];
+	
+	
+	// receive all the possible inputs
+	for (int i = 1; i < in_num; i++) { // starts in 1 because the 0 has already been done
+		cin >> in_a[i];
+		cin >> in_b[i];
+		
+		// update what we need to compute in fibonacci sequence
+		if(in_a[i] < lower) {
+			lower = in_a[i];
+		}
+		
+		if(in_a[i] + in_b[i] > bigger) {
+			bigger = in_a[i] + in_b[i];
+		}
+	} 
+	
+	// Create array to store all the need to compute arrays
+	//long int *fib_arr = new long int[bigger - lower];
+	long int *fib_arr = new long int[bigger];
+	
+	// Compute all of them
+	fibArr(lower, bigger, fib_arr);
+	
+	for(int i = 0; i < in_num - 1; i++) { // clone, order and print all of them.
+		// Generate the array that will be ploted now
+		long int *p_arr = new long int[bigger - lower];
+		int sum = in_a[i] + in_b[i];
+		
+		for(int j = in_a[i]; j < sum; j++) {
+			p_arr[j - in_a[i]] = fib_arr[j];
+		}
+		
+		// print p_arr
+		printArr(p_arr, sum);
+		
+		delete[] p_arr;
+	}
+	
+	printArr(fib_arr, bigger - lower);
+	
+	
+	delete[] in_a;
+	delete[] in_b;
+	
+	return 0;
+}
+
+
+long int fib(int pos) {
+	long int fib_val = 0;
+	int old[] = {0,1};
+	
+	if(pos == 1) {
+		fib_val = 0;
+	}
+	else if(pos == 2) {
+		fib_val = 1;
+	}
+	
+	else {
+		
+		for (int i = 1; i < pos; i++) {
+			fib_val = old[0] + old[1];
+			old[0] = old[1];
+			old[1] = fib_val;
+		}
+		
+	}
+	
+	return fib_val;
+}
+
+
+long int fibArr(int begin, int end, long int *arr) {
+	long int fib_val = 0;
+	int old[] = {0,1};
+	begin = 1;
+	
+	for (int i = 1; i < end; i++) {
+		
+		fib_val = (old[0] + old[1])%10000;
+		old[0] = old[1];
+		old[1] = fib_val;
+		
+		if(i >= begin) { // records the necessary fibonacci values
+			arr[i - begin] = fib_val;
+		}
+	}
+	
+	return fib_val;
+}
+
+void printArr(long int *arr, int size) {
+	if(size > 100) size = 100;
+	
+	for (int i = 0; i < size ; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
