547506cbc9e0bbd980f2990a51f579b13b5f85c2
Caio Moreira Gomes <caiogomes34@gmail.com>
2017-11-02 05:27:01 -0300

    Preparação para fazer a lista 5.

---
+++ algorithms/lists/list_5/q1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+class graph(){
+public:
+	vector< vector< pair<int, int> > > v; // vertex contains w and u as adjacency list
+
+	graph();
+	~graph();
+
+	insert_vertex(int u, int v, int w) {
+		
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	
+
+	return 0;
+}
+
+void dijkstra() {
+
+}
