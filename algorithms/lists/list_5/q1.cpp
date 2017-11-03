dfde64c7f03faacb1e2d18d9e37182058155e756
Caio Moreira Gomes <caiogomes34@gmail.com>
2017-11-02 21:38:18 -0300

    Resolução da ficha de algorítmos, a questão 2 não passa por dar TLE, necessário criar priority_queue e remover o sort.

--- algorithms/lists/list_5/q1.cpp
+++ algorithms/lists/list_5/q1.cpp
@@ -1,29 +1,8 @@
 #include <bits/stdc++.h>
 
-<<<<<<< HEAD
-class graph(){
-public:
-	vector< vector< pair<int, int> > > v; // vertex contains w and u as adjacency list
-
-	graph();
-	~graph();
-
-	insert_vertex(int u, int v, int w) {
-		
-	}
-}
-
-int main(int argc, char const *argv[]) {
-	
-
-	return 0;
-}
-
-void dijkstra() {
-
-=======
 #define endl '\n'
-#define MAX_SIZE 200000
+#define MAX_SIZE 100000
+#define INF 0x3f3f3f3f
 
 using namespace std;
 
@@ -31,11 +10,13 @@ class graph {
 public:
 	vector< pair<int, int> > e[MAX_SIZE];
 
-	graph() {
+	graph() {}
+	~graph(){}
 
-	}
-	~graph(){
-
+	void clean() {
+		for (int i = 0; i < MAX_SIZE; ++i) {
+			e[i].clear();
+		}
 	}
 
 	void print_e() {
@@ -66,19 +47,79 @@ public:
 		return 0;
 	}
 
-	void dijkstra(int v) {
-		// create e clone with height inf and the beginning border with wight as 0
+	// bool update_w(vector< pair<int, int> > &v, int new_w, int value) {
+	bool update_w(pair<int, int> *v, int new_w, int value) {
+		bool ret = 0;
+		// for (int i = 0; i < v.size(); ++i) {
+		// 	if(v[i].second == value) {
+		// 		if(new_w < v[i].first) {
+		// 			v[i].first = new_w;
+		// 			ret = 1;
+		// 		}
+		// 		break;
+		// 	}
+		// }
+		if(new_w < v[value].first) {
+			v[value].first = new_w;
+			ret = 1;
+		}
 
-		// create a binary vector wich contains the binary representation to every single
-		// line to be connect at it.
+		return ret;
+	}
+
+	pair<int, int> find_pair(vector< pair<int, int> > v, int vertex) {
+		for (int i = 0; i < v.size(); ++i) {
+			if(v[i].second == vertex) {
+				return v[i];
+			}
+		}
+
+		return make_pair(-1, -1);
+	}
+
+	int dijkstra(int v, int ve) {
+		// create e clone with height INF and the beginning border with wight as 0
+		priority_queue< pair<int, int>, vector< pair<int, int> >, greater< pair <int, int> > > pq;
+		// vector< pair<int, int> > dist;
+		pair<int, int> *dist = new pair<int, int>[MAX_SIZE];
+		pair<int, int> temp, buff;
+
+		temp = make_pair(-1, -1); // guarantee the problem will have an output
 
 		// make an priority queue and insert the beginning vertex into it
+		pq.push(make_pair(0, v));
+		// dist.push(make_pair(0, v));
 
-		// process the extracted vertex and update every single adjacent border
+		for (int i = 0; i < MAX_SIZE; ++i) {
+			// if(e[i].size() > 0) {
+				// dist.push_back(make_pair(INF, i));
+				dist[i] = make_pair(INF, i);
+			// }
+		}
+		update_w(dist, 0, v);
 
-		// insert the updated borders into the priority queue
+		while(!pq.empty()) {
+			// get vertex weight
+			// temp = find_pair(dist, pq.top().second);
+			temp = dist[pq.top().second];
 
-		// goes doing it untill the priority queue is empty or the marks become all processed
+			// add the new vertex into the pq
+			for (int i = 0; i < e[pq.top().second].size(); ++i) {
+				
+				int new_w = e[pq.top().second][i].first + pq.top().first;
+				
+				if(update_w(dist, new_w, e[pq.top().second][i].second)) { // update if new weight is lower
+					// temp = find_pair(dist, e[pq.top().second][i].second);
+					temp = dist[e[pq.top().second][i].second];
+					pq.push(temp);
+				}
+			}
+
+			pq.pop();
+		}
+
+		temp = dist[ve];
+		return temp.first;
 	}
 };
 
@@ -87,22 +128,38 @@ int main(int argc, char const *argv[]) {
 	ios::sync_with_stdio(0);
 	int m, n;
 	int a, b, w;
+	int begin_v = 0, end_v;
+	int tc, temp = -1;
 
 	graph g;
 
-	while(cin >> m >> n, m != 0 && n != 0) {
+	cin >> tc;
+
+	// while(cin >> m >> n, m != 0 && n != 0) {
+	for(; tc > 0;tc--) {
 		
+		cin >> m >> n >> begin_v >> end_v;
+
 		for (int i = 0; i < n; ++i) {
 			cin >> a >> b >> w;
-			g.add_e(a, b, w);
-			cout << "a: " << a << " b: " << b << " w: " << w << endl;
+			g.add_e(a, b, w);	
+			// cout << "a: " << a << " b: " << b << " w: " << w << endl;
 		}
 		
+		temp = g.dijkstra(begin_v, end_v);
+
+		if(temp == INF || temp == -1) {
+			cout << "NONE" << endl;
+		}
+		else {
+			cout << temp << endl;
+		}
+
+		g.clean();
 	}
  
-	g.print_e();
-
+	// g.print_e();
+	// g.dijkstra(begin_v);
 
 	return 0;
->>>>>>> 62c5c4b2538d73e83368cb4407dc0db4123db330
 }
---
+++ algorithms/lists/list_5/q2.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define lli long long int
+#define MAX_SIZE 200000
+#define edge_t pair< int, pair< int, int > >
+using namespace std;
+
+bool sort_edges(edge_t a, edge_t b) {
+	return  (a.first < b.first);
+}
+
+void printv(vector<edge_t> v) {
+	for (int i = 0; i < v.size(); ++i) {
+		cout << v[i].first << " ";
+	}
+	cout << endl;
+}
+
+bool DFS(vector<int> g[], bool visits[], int a, int b) {
+	cout << a << " " << b << endl;
+
+	if(visits[a]) { // já visitou, sem caminhos possíveis
+		return 0;
+	}
+
+	if(a == b) {
+		return 1; // entrou no vertice buscado
+	}
+
+	bool sol = 0;
+
+
+	for (int i = 0; i < g[a].size(); ++i) {
+		visits[a] = 1;
+		sol = DFS(g, visits, g[a][i], b);
+		
+		if(sol) {
+			// return 1;
+			break;
+		}
+	}
+
+	visits[a] = 0; // o vértice não tem mais nada a oferecer, desvisita
+
+	return sol;
+}
+
+lli minimum_tree(vector<edge_t> edges) {
+	int visited[MAX_SIZE] = {0};
+	lli min = 0;
+	vector<int> g[MAX_SIZE]; // grafo da minimum spanning tree
+	bool visits[MAX_SIZE] = {0}; // grafo da minimum spanning tree
+
+	for (int i = 0; i < edges.size(); ++i) {
+		// TODO: verificar se existe um ciclo ou não
+		if(visited[edges[i].second.first] && visited[edges[i].second.second]) {
+			// busca um caminho
+			if(DFS(g, visits, edges[i].second.first, edges[i].second.second)) {
+				continue;
+			}
+			cout << "---------" << endl;
+		}
+
+		min += edges[i].first;
+		visited[edges[i].second.first] = visited[edges[i].second.second] = 1;
+		g[edges[i].second.first].push_back(edges[i].second.second);
+		g[edges[i].second.second].push_back(edges[i].second.first);
+	}
+
+	return min;
+}
+
+int main(int argc, char const *argv[]) {
+	ios::sync_with_stdio(0);
+	
+	lli total = 0;
+	lli sum = 0;
+	int m, n;
+	int x, y, z;
+	lli min = 0;
+	
+	vector < edge_t > edges;
+	// priority_queue<edge_t, vector<edge_t>, greater> edges;
+
+	while(cin >> m >> n, n != 0 || m != 0) {
+		total = sum = min = 0;
+		edges.clear();
+
+	    for (int i = 0; i < n; ++i) {
+	    	cin >> x >> y >> z;
+
+	    	total += z;
+	    	edges.push_back(make_pair(z, make_pair(x, y)));
+	    }
+
+	    sort(edges.begin(), edges.end(), sort_edges);
+
+	    printv(edges);
+	    cout << total << endl;
+	    min = minimum_tree(edges);
+	    cout << min << endl;
+	    cout << total - min << endl;
+	}
+
+	return 0;
+}
